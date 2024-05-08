4df45daf8e992a76d22275a5e1e99fa055865af9
CHOI HyoJun <cjfgh2449@gmail.com>
2024-05-08 21:31:42 +0900

    feat. JPA 적용

--- demo/src/main/java/com/example/demo/SpringConfig.java
+++ demo/src/main/java/com/example/demo/SpringConfig.java
@@ -1,21 +1,54 @@
 package com.example.demo;
 
-import com.example.demo.repository.MemberRepository;
-import com.example.demo.repository.MemoryMemberRepository;
+import com.example.demo.repository.*;
 import com.example.demo.service.MemberService;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import javax.swing.text.html.parser.Entity;
 
 @Configuration
 public class SpringConfig {
 
-    @Bean
-    public MemberService memberService(){
-        return new MemberService(memberRepository());
+//
+//    private DataSource dataSource;
+//    private EntityManager em;
+//
+//    public SpringConfig(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
+//
+//
+//    @Autowired
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
+//
+//    @Bean
+//    public MemberService memberService(){
+//        return new MemberService(memberRepository());
+//    }
+//
+//    @Bean
+//    public MemberRepository memberRepository(){
+////        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JpaMemberRepository(em);
+//    }
+
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
     }
 
     @Bean
-    public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+    public MemberService memberService(){
+        return new MemberService(memberRepository);
     }
+
 }
--- demo/src/main/java/com/example/demo/service/MemberService.java
+++ demo/src/main/java/com/example/demo/service/MemberService.java
@@ -5,20 +5,22 @@ import com.example.demo.repository.MemberRepository;
 import com.example.demo.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
+@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
+
 
     //생성자를 통한 DI
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
 // 회원 가입
-
     public Long join(Member member){
         //같은 이름이 있는 중복 회원 X
         validateDuplicateMember(member); // ctrl+alt+m 으로 메소드 생성
